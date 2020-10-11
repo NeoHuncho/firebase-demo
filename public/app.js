3c4939d6817559bce1a58725cc007089e433397d
NeoHuncho <william.guinaudie@gmail.com>
2020-10-11 23:56:04 +0200

    added sign in id

--- public/app.js
+++ public/app.js
@@ -1,4 +1,7 @@
+///// User Authentication /////
+
 const auth = firebase.auth();
+
 const whenSignedIn = document.getElementById('whenSignedIn');
 const whenSignedOut = document.getElementById('whenSignedOut');
 
@@ -6,6 +9,8 @@ const signInBtn = document.getElementById('signInBtn');
 const signOutBtn = document.getElementById('signOutBtn');
 
 const userDetails = document.getElementById('userDetails');
+
+
 const provider = new firebase.auth.GoogleAuthProvider();
 
 /// Sign in event handlers
@@ -13,3 +18,17 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
